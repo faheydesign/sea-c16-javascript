bfe4e55040cf76316b19977bf69976a9f5b014e0
John Fahey <faheydesign@gmail.com>
2014-07-28 17:49:04 -0700

    adding assignment 5.1

--- students/johnfahey/assignment5.1.js
+++ students/johnfahey/assignment5.1.js
@@ -1,11 +1,57 @@
-function oldestAge(peeps) {
-   return _.max(peeps, 'age').age;
+// Given the following data structure
+// implement a oldestLivingParent method
+// that will return the name of the oldest
+// living parent.
+
+var people = [
+  {
+    name: 'Hank',
+    age: 29,
+    parent: 'Don'
+  },
+  {
+    name: 'Deborah',
+    age: 30,
+    parent: 'Don'
+  },
+  {
+    name: 'Danny',
+    age: 24,
+    parent: 'Don'
+  },
+  {
+    name: 'Don',
+    age: 64,
+    parent: 'Haskell'
+  },
+  {
+    name: 'Kathy',
+    age: 75,
+    parent: 'Lenora'
+  },
+  {
+    name: 'Haskell',
+    parent: 'Finus'
+  },
+  {
+    name: 'Finus'
+  }
+];
+
+
+function oldestLivingParent(peeps){
+  var living = [];
+  for(var i = 0; i < peeps.length; i++){
+    if (peeps[i].age){
+      for(var j = 0; j < peeps.length; j++){
+        if ( peeps[i].name == peeps[j].parent){
+          return peeps[i].name;
+        }
+      }
+    }
+  }
 }
 
-function living(peeps) {
-    return _.flatten( _.filter(peeps, 'age'),'name');
-}
 
-function oldestLivingPerson(peeps){
-  return _.max(peeps, 'age').name;
-}
+oldestLivingParent(people);
+// -> 'Don'
